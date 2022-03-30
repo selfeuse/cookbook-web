3b877182a00f6dd8265a1ba65fbe492e09ce0b77
Cassandre <cassandre.pochet@epitech.eu>
2022-03-30 11:08:12 -0400

    Fix of click on the card and quick desgin improvement

--- client/src/components/Recipe/Recipes/Recipe/styles.js
+++ client/src/components/Recipe/Recipes/Recipe/styles.js
@@ -2,16 +2,9 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles({
   media: {
-    height: 0,
-    paddingTop: '56.25%',
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
-    backgroundBlendMode: 'darken',
-  },
-  border: {
-    border: 'solid',
-  },
-  fullHeightCard: {
-    height: '100%',
+    borderRadius: '15px',
+    margin: '5px',
+    width: 'auto'
   },
   card: {
     display: 'flex',
@@ -21,36 +14,16 @@ export default makeStyles({
     height: '100%',
     position: 'relative',
   },
-  overlay: {
-    position: 'absolute',
-    top: '20px',
-    left: '20px',
-    color: 'white',
-  },
-  overlay2: {
-    position: 'absolute',
-    top: '20px',
-    right: '20px',
-    color: 'white',
-  },
-  grid: {
-    display: 'flex',
-  },
-  details: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    margin: '20px',
-  },
   title: {
-    padding: '0 16px',
+    fontWeight: 600
   },
   cardActions: {
     padding: '0 16px 8px 16px',
     display: 'flex',
     justifyContent: 'space-between',
   },
-  cardAction: {
-    display: 'block',
-    textAlign: 'initial',
-  },
+  goToButton: {
+    right: '20px',
+    bottom: '10px',
+  }
 });
